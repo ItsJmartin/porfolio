fbf987b138dd1e6434160e8628e8d76384745196
Joseph Martin <jmartinsayed@gmail.com>
2024-10-29 17:03:37 +0530

    update 1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function () {
+
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Flutter Developer", "Mobile Application Developer", "Cross-Platform Developer"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+
+    });
+
+});
+
